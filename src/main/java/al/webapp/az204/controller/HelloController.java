2853978623d956cacbd175f27ad5be6f8fc43dd3
Ardit Azo <arditazo3@gmail.com>
2020-12-27 13:56:37 +0100

    First commit

---
+++ src/main/java/al/webapp/az204/controller/HelloController.java
@@ -0,0 +1,14 @@
+package al.webapp.az204.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ src/test/java/al/webapp/az204/Az204ApplicationTests.java
@@ -0,0 +1,13 @@
+package al.webapp.az204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Az204ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
