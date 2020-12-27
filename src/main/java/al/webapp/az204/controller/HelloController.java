5fe65c0c1b51fcef8656d2aec69e6fa3da997f58
Ardit Azo <arditazo3@gmail.com>
2020-12-27 21:38:45 +0100

    adding lombok

--- src/main/java/al/webapp/az204/controller/HelloController.java
+++ src/main/java/al/webapp/az204/controller/HelloController.java
@@ -1,13 +1,18 @@
 package al.webapp.az204.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
+
+        log.info("Hit the endpoint!");
+
         return "Pershendetje nga Azure App Service, &#10084; ime!";
     }
 
